4e7d399d94d6c57e12e26a8e1e560b4e720bb421
John Scira <jscira@broadinstitute.org>
2024-09-20 11:33:22 -0400

    added support for boolean flags

--- dev/transform_json_to_input_args.py
+++ dev/transform_json_to_input_args.py
@@ -14,5 +14,14 @@ if __name__ == '__main__':
     with open(args.input_json) as f:
         input_json = json.load(f)
 
-    formatted_args = [f"--{key[key.find('.')+1:]} {value} " for key, value in input_json.items()]
+    for arg in input_json:
+        boolean_args = []
+        value = arg.value.lower()
+        if value in ['true', 'false']:
+            if value == 'true':
+                boolean_args.append(f"--{arg.key[arg.key.find('.')+1:]} ")
+            input_json.pop(arg.key)
+
+    non_boolean_args = [f"--{key[key.find('.')+1:]} {value} " for key, value in input_json.items()]
+    formatted_args = boolean_args + non_boolean_args
     print(''.join(formatted_args))
