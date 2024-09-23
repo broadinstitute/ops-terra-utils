3a1094094455716d9c269813d624fa765ac3f157
Nareh Sahakian <sahakian@broadinstitute.org>
2024-09-23 12:07:31 -0400

    update args script

--- dev/transform_json_to_input_args.py
+++ dev/transform_json_to_input_args.py
@@ -3,8 +3,7 @@ from argparse import ArgumentParser, Namespace
 
 
 def get_args() -> Namespace:
-    parser = ArgumentParser(
-        description="Transform template input json to input arguments")
+    parser = ArgumentParser(description="Transform template input json to input arguments")
     parser.add_argument("-i", "--input_json", required=True)
     return parser.parse_args()
 
@@ -14,14 +13,11 @@ if __name__ == '__main__':
     with open(args.input_json) as f:
         input_json = json.load(f)
 
-    for arg in input_json:
-        boolean_args = []
-        value = arg.value.lower()
-        if value in ['true', 'false']:
-            if value == 'true':
-                boolean_args.append(f"--{arg.key[arg.key.find('.')+1:]} ")
-            input_json.pop(arg.key)
-
-    non_boolean_args = [f"--{key[key.find('.')+1:]} {value} " for key, value in input_json.items()]
-    formatted_args = boolean_args + non_boolean_args
-    print(''.join(formatted_args))
+    formatted_args = []
+    for k, v in input_json.items():
+        param_name = k.split(".")[1]
+        if v in ["true", "false"]:
+            formatted_args.append(f"--{param_name}")
+        else:
+            formatted_args.append(f"--{param_name} {v}")
+    print(" ".join(formatted_args))
