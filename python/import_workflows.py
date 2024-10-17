ad31bc643ddb3adbd6af9500f7e3e732d9cc5fa1
Nareh Sahakian <45041478+sahakiann@users.noreply.github.com>
2024-10-17 12:09:42 -0400

    POD-2213: Add Terra Utils Integration Tests (#90)
    * POD-2213: Terra Integration Tests
    
    * undo changes for testing
    
    * update logging
    
    * add tests for groups
    
    * reuse gcp object
    
    * delete group before attempting to run integration tests

--- python/import_workflows.py
+++ python/import_workflows.py
@@ -41,6 +41,14 @@ if __name__ == '__main__':
         if not workflow_already_in_workspace(workflow_name=workflow, workspace_workflows=imported_workflows):
             logging.info(f"Importing {workflow} into {args.billing_project}/{args.workspace_name}")
             workflow_config = getattr(WorkflowConfigs(), workflow)(billing_project=args.billing_project)
-            method_import = workspace.import_workflow(workflow_dict=workflow_config)
+            status_code = workspace.import_workflow(workflow_dict=workflow_config)
+            if status_code == 201:
+                logging.info(
+                    f"Successfully started import for workflow '{workflow}' into workspace '{args.workspace_name}'"
+                )
+            else:
+                logging.info(
+                    f"Got the following status code when attempting to import workflow '{workflow}': '{status_code}'"
+                )
         else:
             logging.info(f"{workflow} already in workspace, skipping import")
