d2aa8f6b8d733e10535ad4f16e5533e80d525ce7
John Scira <jscira@broadinstitute.org>
2024-10-17 14:32:31 -0400

    adding resource type info

--- python/tests/integration_tests/test_tdr_utils.py
+++ python/tests/integration_tests/test_tdr_utils.py
@@ -8,63 +8,81 @@ from python.utils.tdr_utils.tdr_api_utils import TDR
 from python.utils.tdr_utils.tdr_table_utils import SetUpTDRTables
 from python.utils.tdr_utils.tdr_schema_utils import InferTDRSchema
 
-
+# NOTES: 
+# test dataset created for any tests that just need to get info 
+# ops_integration_test_dataset - dataset id: 0981274b-61e3-4efb-99f2-eaea57075612
 @pytest.fixture(scope='session', autouse=True)
 def setup_tdr_resources() -> Any:
     pass
 
 
-
 def test_get_data_set_files():
     cmd = TDR().get_data_set_files(dataset_id=' str')
+
 
 def test_create_file_dict():
     cmd = TDR().create_file_dict(dataset_id='str')
 
+
 def test_get_sas_token():
     cmd = TDR().get_sas_token(snapshot_id=' str', dataset_id='str')
+
 
 def test_delete_file():
     cmd = TDR().delete_file(file_id='str', dataset_id='str')
 
+
 def test_delete_files():
     cmd = TDR().delete_files(file_ids=list[str], dataset_id=str)
+
 
 def test_add_user_to_dataset():
     cmd = TDR().add_user_to_dataset(dataset_id=str, user=str, policy=str)
 
+
 def test_delete_dataset():
     cmd = TDR().delete_dataset(dataset_id=str)
+
 
 def test_get_snapshot_info():
     cmd = TDR().get_snapshot_info(snapshot_id=str)
 
+
 def test_delete_snapshots():
     cmd = TDR().delete_snapshots(snapshot_ids=list[str])
+
 
 def test_delete_snapshot():
     cmd = TDR().delete_snapshot(snapshot_id=str)
 
+
 def test_check_if_dataset_exists():
     cmd = TDR().check_if_dataset_exists(dataset_name=str, billing_profile=str)
+
 
 def test_get_dataset_info():
     cmd = TDR().get_dataset_info(dataset_id=str)
 
+
 def test_get_table_schema_info():
     cmd = TDR().get_table_schema_info(dataset_id=str, table_name=str)
+
 
 def test_ingest_to_dataset():
     cmd = TDR().ingest_to_dataset(dataset_id=str, data=dict)
 
+
 def test_get_data_set_table_metrics():
     cmd = TDR().get_data_set_table_metrics(dataset_id=str, target_table_name=str)
+
 
 def test_get_data_set_sample_ids():
     cmd = TDR().get_data_set_sample_ids(dataset_id=str, target_table_name=str, entity_id=str)
 
+
 def test_get_data_set_file_uuids_from_metadata():
     cmd = TDR().get_data_set_file_uuids_from_metadata(dataset_id=str)
+
 
 def test_get_or_create_dataset():
     cmd = TDR().get_or_create_dataset(
@@ -75,6 +93,7 @@ def test_get_or_create_dataset():
         cloud_platform=str
     )
 
+
 def test_update_dataset_schema():
     cmd = TDR().update_dataset_schema(  # type: ignore[return]
         dataset_id=str,
@@ -84,10 +103,9 @@ def test_update_dataset_schema():
         columns_to_add='Optional[list[dict]]'
     )
 
+
 def test_get_files_from_snapshot():
     cmd = TDR().get_files_from_snapshot(snapshot_id=str)
-
-
 
 
 def test_ingest():
@@ -97,6 +115,7 @@ def test_ingest():
 def test_SetUpTDRTables():
     cmd = SetUpTDRTables().run()
     pass
+
 
 def test_InferTDRSchema():
     cmd = InferTDRSchema().infer_schema()
