d6ffb33087757cd8e9c4688576547f7eb1a693b4
John Scira <jscira@broadinstitute.org>
2024-11-05 16:42:41 -0500

    adding new test case for function to get blob details

--- python/tests/integration_tests/test_google_utils.py
+++ python/tests/integration_tests/test_google_utils.py
@@ -83,6 +83,11 @@ def test_list_bucket_contents() -> None:
     result = GCPCloudFunctions().list_bucket_contents(bucket_name=gcp_test_resource_json()["bucket"])
     assert len(result) == gcp_blob_count, f"Expected {gcp_blob_count} files, got {len(result)}"
 
+def test_get_blob_details() -> None:
+    test_data = gcp_test_resource_json()['tests']['get_blob_details']['test_data']
+    result = GCPCloudFunctions().get_blob_details(cloud_path=test_data['function_input']['blob_path'])
+    assert result.path == "/b/ops_dev_bucket/o/list_bucket_test%2Fex_file_1.txt"
+
 
 def test_copy_cloud_file() -> None:
     test_data = gcp_test_resource_json()['tests']['copy_file']['test_data']
