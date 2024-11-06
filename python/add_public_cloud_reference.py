ee1b20e7e8afa63e9962058cff3fe2240ea4d043
Nareh Sahakian <sahakian@broadinstitute.org>
2024-11-06 12:26:46 -0500

    use existing utils

--- python/add_public_cloud_reference.py
+++ python/add_public_cloud_reference.py
@@ -1,9 +1,10 @@
 import argparse
 import os
 import logging
-from google.cloud import storage
 from urllib.parse import urlparse
 from pathlib import Path
+
+from utils.gcp_utils import GCPCloudFunctions
 
 
 class CopyPublicCloudReference:
@@ -26,10 +27,9 @@ class CopyPublicCloudReference:
         self.bwa_mem_tar_file_location = bwa_mem_tar_file_location
         self.output_cloud_path = output_cloud_path
         self.read_me_path = read_me_path
-        # The default SA credentials are set in the cloud run job under "Security" -> "Service Account": https://console.cloud.google.com/run/jobs/edit/us-central1/add-public-cloud-reference?project=operations-portal-427515&authuser=0  # noqa: E501
-        self.client = storage.Client()
 
         self.output_bucket_root_dir = self._get_output_bucket_location()
+        self.gcp = GCPCloudFunctions()
 
     def _get_output_bucket_location(self) -> str:
         parsed_url = urlparse(self.output_cloud_path)
@@ -42,26 +42,6 @@ class CopyPublicCloudReference:
 
     def _get_readme_file_output_path(self) -> str:
         return os.path.join(self.output_bucket_root_dir, "README.txt")
-
-    def _copy_source_file_to_destination(self, source_path: str, destination_path: str) -> None:
-        src_parsed = urlparse(source_path)
-        source_bucket_name = src_parsed.netloc
-        source_blob_name = src_parsed.path.lstrip("/")
-
-        dest_parsed = urlparse(destination_path)
-        destination_bucket_name = dest_parsed.netloc
-        destination_blob_name = dest_parsed.path.lstrip("/")
-
-        source_bucket = self.client.bucket(source_bucket_name)
-        source_blob = source_bucket.blob(source_blob_name)
-
-        destination_bucket = self.client.bucket(destination_bucket_name)
-
-        source_bucket.copy_blob(
-            source_blob,
-            destination_bucket,
-            destination_blob_name
-        )
 
     def copy_files_to_public_bucket(self) -> None:
         gcs_files_to_copy = [
@@ -76,13 +56,13 @@ class CopyPublicCloudReference:
             logging.info("Copying references files from source location to Broad bucket")
             for source_file in gcs_files_to_copy:
                 destination_path = self._get_file_output_path(source_file=source_file)
-                self._copy_source_file_to_destination(source_path=source_file, destination_path=destination_path)
+                self.gcp.copy_cloud_file(src_cloud_path=source_file, full_destination_path=destination_path)
                 logging.info(f"Successfully copied '{source_file}' to '{destination_path}'\n")
 
             # Copy README
             logging.info("Copying README to public cloud reference bucket")
             read_me_destination = self._get_readme_file_output_path()
-            self._copy_source_file_to_destination(source_path=self.read_me_path, destination_path=read_me_destination)
+            self.gcp.copy_cloud_file(src_cloud_path=self.read_me_path, full_destination_path=read_me_destination)
             logging.info(f"Successfully copied README file to '{read_me_destination}'")
 
         except Exception as e:
