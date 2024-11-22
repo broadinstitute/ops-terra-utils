e18b6950fd6def0ff730fa7fbc76138d69ddb7bb
John Scira <jscira@broadinstitute.org>
2024-11-22 17:03:01 -0500

    removing unused class

--- python/copy_az_terra_files_to_gcp_bucket.py
+++ python/copy_az_terra_files_to_gcp_bucket.py
@@ -34,53 +34,6 @@ def get_args() -> Namespace:
     return parser.parse_args()
 
 
-class AzureToGoogleFileTransfer():
-    def __init__(self, blob_list, gcp_bucket, gcp_client, az_accnt_url, az_container, workspace_client, temp_dir):
-        self.blob_list = blob_list
-        self.export_bucket = gcp_bucket
-        self.gcp_client = gcp_client
-        self.az_accnt_url = az_accnt_url
-        self.az_container = az_container
-        self.workspace_client = workspace_client
-        self.temp_dir = temp_dir
-        self.sas_token = None
-
-    def format_upload_path(self, blob_path):
-        bucket_str = self.export_bucket
-        if not bucket_str.startswith('gs://'):
-            bucket_str = f"gs://{bucket_str}"
-        if not bucket_str.endswith('/'):
-            bucket_str = f"{bucket_str}/"
-        return f"{bucket_str}{blob_path}"
-
-    def blob_exists(self, upload_path):
-        return GCPCloudFunctions().get_blob_details(upload_path)
-
-    def delete_file_after_transfer(self, file_path):
-        try:
-            file_path.unlink()
-        except Exception as e:
-            logging.error(f"Error deleting file {file_path}: {e}")
-
-    def run(self):
-        self.sas_token = self.workspace_client.retrieve_sas_token(2400)
-        token_util = SasTokenUtil(token=self.sas_token)
-        tmp_dir = Path(self.temp_dir)
-        for blob in self.blob_list:
-            if token_util.seconds_until_token_expires() < 600:
-                self.sas_token = self.workspace_client.retrieve_sas_token(2400)
-            upload_path = self.format_upload_path(blob['relative_path'])
-            if not self.blob_exists(upload_path):
-                dl_path = tmp_dir.joinpath(blob['file_name'])
-                blob_client = AzureBlobDetails(account_url=self.az_accnt_url,
-                                               sas_token=self.sas_token,
-                                               container_name=self.az_container)
-                blob_client.download_blob(blob_name=blob['relative_path'], dl_path=dl_path)
-                self.gcp_client.upload_blob(destination_path=upload_path, source_file=dl_path)
-                self.delete_file_after_transfer(dl_path)
-
-        tmp_dir.rmdir()
-
 
 class RunExport():
     def __init__(self, gcp_client, workspace_client, temp_dir):
