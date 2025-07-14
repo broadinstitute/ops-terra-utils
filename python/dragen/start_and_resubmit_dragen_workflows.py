fe472db7c0c531a0744a6ef8a3dbebea0167d744
Nareh Sahakian <sahakian@broadinstitute.org>
2025-07-14 12:30:21 -0400

    small fixes to logic

--- python/dragen/start_and_resubmit_dragen_workflows.py
+++ python/dragen/start_and_resubmit_dragen_workflows.py
@@ -4,7 +4,7 @@ Script to manage Dragen workflow submissions and resubmissions in Terra workspac
 
 import argparse
 import logging
-import sys
+import csv
 from typing import Union
 
 from ops_utils.token_util import Token
@@ -12,49 +12,54 @@ from ops_utils.request_util import RunRequest
 from ops_utils.terra_util import TerraWorkspace
 from ops_utils.gcp_utils import GCPCloudFunctions
 
-from python.dragen.dragen_utils import GetSampleInfo, CreateSampleTsv, SAMPLE_TSV
+from dragen.dragen_utils import GetSampleInfo, CreateSampleTsv, SAMPLE_TSV
 
 
-# Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
 MAX_SAMPLES_PER_BATCH = 2000
 RUNNING_STATUS = "RUNNING"
 FAILED_STATUS = "FAILED"
+STATUS_COLUMN_HEADER = "latest_status"
 
 
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Start and resubmit Dragen workflows in Terra.")
-    parser.add_argument('--research_project', required=True)
-    parser.add_argument('--ref_trigger_path', required=True)
-    parser.add_argument('--ref_dragen_config', required=True)
-    parser.add_argument('--ref_batch_config', required=True)
-    parser.add_argument('--output_bucket', required=True)
-    parser.add_argument('--google_project_id', required=True)
-    # TODO: Add options for valid data types
-    parser.add_argument('--data_type', required=True)
-    parser.add_argument('--dragen_version', required=True)
-    parser.add_argument('--billing_project', required=True)
-    parser.add_argument('--workspace_name', required=True)
+    parser.add_argument("--ref_trigger_path", required=True, help="GCP path to the reference trigger file")
+    parser.add_argument("--ref_dragen_config", required=True, help="GCP path to the reference Dragen config file")
+    parser.add_argument("--ref_batch_config", required=True, help="GCP path to the reference batch config file")
+    parser.add_argument("--output_bucket", required=True,
+                        help="GCS bucket to store output files. Do not include 'gs://' prefix.")
+    parser.add_argument("--google_project_id", required=True,
+                        help="The Google Project ID where the Dragen workflows will run.")
+    parser.add_argument("--data_type", required=True, help="Data type for the Dragen samples",
+                        choices=["wgs", "bge", "rna"])
+    parser.add_argument("--dragen_version", required=True, help="The Dragen pipeline version")
+    parser.add_argument("--billing_project", required=True, help="The Terra workspace's billing project")
+    parser.add_argument("--workspace_name", required=True, help="The Terra workspace name")
     parser.add_argument(
-        '--samples_per_batch',
+        "--samples_per_batch",
         type=int,
         default=MAX_SAMPLES_PER_BATCH,
-        required=False
+        required=False,
+        help=f"The maximum number of samples per batch to run. Defaults to {MAX_SAMPLES_PER_BATCH}",
     )
     parser.add_argument(
-        '--batch_processing_start_date',
+        "--batch_processing_start_date",
         required=True,
-        help="Start date for batch processing. Should be in the format YYYY-MM-DD"
+        help="Start date for batch processing. Should be in the format YYYY-MM-DD",
     )
     parser.add_argument(
-        '--batch_processing_end_date',
+        "--batch_processing_end_date",
         required=False,
         default="2035-10-30",
         help="End date for batch processing. Default is 2035-10-30. If provided, should be in the format YYYY-MM-DD"
     )
     parser.add_argument(
-        "--sample_id_column", help="Column to use as sample ID input for WDL (for example, `sample_id`, or `collaborator_sample_id`")
+        "--sample_id_column",
+        required=True,
+        help="Column to use as sample ID input for WDL (for example, `sample_id`, or `collaborator_sample_id`")
+
     return parser.parse_args()
 
 
@@ -70,54 +75,73 @@ class FindSamplesForSubmission:
         """
         return [
             row for row in self.workspace_metadata
-            if row.get("status") == RUNNING_STATUS
+            if row["attributes"].get(STATUS_COLUMN_HEADER) == RUNNING_STATUS
         ]
 
-    def _create_batch(self) -> Union[None, list[dict]]:
+    def _create_batch(self) -> Union[None, tuple[str, list[dict]]]:
         """
         Create a batch of samples for submission.
+        Only samples from a single research project (RP) are included in each batch.
+        Prioritize non-submitted samples, and top up from failed samples if needed.
         """
-        # Find samples that have not been submitted yet
-        not_submitted_samples = [row for row in self.workspace_metadata if not row.get("status")]
+        # Find all unique research projects (RPs) in workspace_metadata
+        research_projects = sorted(set(row["attributes"]["rp"] for row in self.workspace_metadata))
 
-        # Find samples that have failed
-        failed_samples = [row for row in self.workspace_metadata if row.get("status") == FAILED_STATUS]
+        for rp in research_projects:
+            # Filter samples by this research project
+            samples_for_rp = [row for row in self.workspace_metadata if row["attributes"]["rp"] == rp]
 
-        if not (not_submitted_samples or failed_samples):
-            logging.info("Found zero samples to submit or resubmit. All samples have been submitted successfully")
-            sys.exit()
-        else:
-            # Calculate how many failed samples to include
+            # Split into not-submitted and failed samples for this rp
+            not_submitted_samples = [row for row in samples_for_rp if not row["attributes"].get(STATUS_COLUMN_HEADER)]
+            failed_samples = [row for row in samples_for_rp if row["attributes"].get(
+                STATUS_COLUMN_HEADER) == FAILED_STATUS]
+
+            # Nothing to submit for this rp; try next rp
+            if not (not_submitted_samples or failed_samples):
+                continue
+
+            # Figure out how many failed samples to top up with
             num_needed_from_failed = self.samples_per_batch - len(not_submitted_samples)
 
             if num_needed_from_failed > 0:
                 batch = not_submitted_samples + failed_samples[:num_needed_from_failed]
                 logging.info(
-                    f"Selected {len(not_submitted_samples)} not-submitted samples "
+                    f"RP '{rp}': Selected {len(not_submitted_samples)} not-submitted samples "
                     f"and {min(num_needed_from_failed, len(failed_samples))} failed samples "
                     f"for a total of {len(batch)}."
                 )
             else:
                 batch = not_submitted_samples[:self.samples_per_batch]
                 logging.info(
-                    f"Selected {self.samples_per_batch} not-submitted samples and 0 failed samples "
-                    f"for a total of {len(batch)}."
+                    f"RP '{rp}': Selected {self.samples_per_batch} not-submitted samples and 0 failed samples "
+                    f"for a total of {len(batch)} samples."
                 )
 
-            samples_and_cram_paths = [
-                {
-                    "sample_id": row[self.sample_id_column],
-                    "cram_path": row["cram_path"]
-                }
-                for row in batch
-            ]
+            if batch:
+                if self.sample_id_column == "sample_id":
+                    samples_and_cram_paths = [{
+                        "sample_id": row["name"],
+                        "cram_path": row["attributes"]["cram_path"]
+                    } for row in batch]
+                else:
+                    samples_and_cram_paths = [
+                        {
+                            "sample_id": row["attributes"][self.sample_id_column],
+                            "cram_path": row["attributes"]["cram_path"]
+                        } for row in batch
+                    ]
+                # We only ever want to return ONE batch, so we can return here
+                return rp, samples_and_cram_paths
 
-            return samples_and_cram_paths
+        # If the loop finishes, and we never returned: no batch found
+        logging.info("No samples to submit across any research project")
+        return None
 
-    def create_sample_batch(self) -> Union[None, list[dict]]:
-        if self._find_running_samples():
+    def create_sample_batch(self) -> Union[None, tuple[str, list[dict]]]:
+        running_samples = self._find_running_samples()
+        if running_samples:
             raise Exception(
-                f"Found {len(self._find_running_samples())} samples that are still running. Will not submit new samples."
+                f"Found {len(running_samples)} samples that are still running. Will not submit new samples."
             )
         else:
             return self._create_batch()
@@ -152,15 +176,16 @@ class DragenConfigGenerator:
         batch_config_filename = "batch_config.json"
 
         # Replace the necessary contents in the Dragen config
-        with open(dragen_config_filename, "r") as dragen_config:
-            updated_contents = dragen_config_contents.replace("__OUT_PATH__", f'"{output_path}"')
+        with open(dragen_config_filename, "w") as dragen_config:
+            updated_contents = dragen_config_contents.replace("__OUT_PATH__", f'{output_path}')
             dragen_config.write(updated_contents)
 
         # Replace the necessary contents in the batch config
-        with open(batch_config_filename, "r") as batch_config:
-            updated_contents = batch_config_contents.replace("__DATA_TYPE__", f'"{self.data_type}"')
+        with open(batch_config_filename, "w") as batch_config:
+            updated_contents = batch_config_contents.replace("__DATA_TYPE__", f'{self.data_type}')
             batch_config.write(updated_contents)
 
+        logging.info("Successfully wrote updated Dragen and batch config files")
         return dragen_config_filename, batch_config_filename
 
 
@@ -267,28 +292,28 @@ if __name__ == "__main__":
     workspace_metrics = workspace.get_gcp_workspace_metrics(entity_type="sample", remove_dicts=True)
 
     # Create a batch of samples for submission to Batch
-    dragen_sample_batch = FindSamplesForSubmission(
+    research_project, dragen_sample_batch = FindSamplesForSubmission(  # type: ignore[misc]
         workspace_metadata=workspace_metrics,
         samples_per_batch=args.samples_per_batch,
         sample_id_column=args.sample_id_column
     ).create_sample_batch()
 
     if dragen_sample_batch:
-        # Write the batch sample manifest
-        # Open the file and write the header
         sample_manifest_filename = "sample_processing_manifest.txt"
+
+        # Write the batch sample manifest
+        headers = [f for f in dragen_sample_batch[0].keys()]
         with open(sample_manifest_filename, "w") as f:
-            f.write("collaborator_sample_id cram_path\n")
-            # Write each row
-            for sample in dragen_sample_batch:
-                f.write(f"{sample[args.sample_id_column]} {sample['cram_path']}\n")
+            writer = csv.DictWriter(f, fieldnames=headers, delimiter=" ")
+            writer.writeheader()
+            writer.writerows(dragen_sample_batch)
 
         # Generate the Dragen Config files
         dragen_config_file, batch_config_file = DragenConfigGenerator(
             ref_dragen_config_gcs=args.ref_dragen_config,
             ref_batch_config_gcs=args.ref_batch_config,
             output_bucket=gsc_output_bucket,
-            research_project=args.research_project,
+            research_project=research_project,
             data_type=args.data_type,
             gcp_util=gcp_util,
         ).generate_updated_dragen_and_batch_configs()
